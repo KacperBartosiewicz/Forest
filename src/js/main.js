e8b1b2f67c182820d129432264ebeb51ec99b590
Kacper <bartosiewiczK@interia.pl>
2025-04-05 23:30:21 +0200

    added 2 classes to nav__btn active and unactive, added active class to nav__links, changed js- added which controled navigation

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+const mobileNav = document.querySelector('.nav__links--mobile')
+const openBtn = document.querySelector('.nav__btn--open')
+const closeBtn = document.querySelector('.nav__btn--close')
+const windowMedia = window.matchMedia('(min-width: 992px)')
+const allMobileNavLinks = mobileNav.querySelectorAll('.nav__link')
+console.log(allMobileNavLinks)
+
+const showNav = () => {
+	mobileNav.classList.toggle('nav__links--active')
+	openBtn.classList.toggle('unactive')
+	closeBtn.classList.toggle('active')
+	allMobileNavLinks.forEach(item => {
+		item.addEventListener('click', () => {
+			mobileNav.classList.remove('nav__links--active')
+		})
+	})
+}
+
+const watchWindow = () => {
+	openBtn.classList.add('unactive')
+	closeBtn.classList.add('unactive')
+	mobileNav.classList.remove('nav__links--active')
+	if (window.innerWidth < 992) {
+		openBtn.classList.remove('unactive')
+		closeBtn.classList.remove('unactive')
+		closeBtn.classList.remove('active')
+	}
+}
+
+openBtn.addEventListener('click', showNav)
+closeBtn.addEventListener('click', showNav)
+windowMedia.addEventListener('change', watchWindow)
