9be8bcb74a8b58fa80282847c833b2c3806266a1
Kacper <bartosiewiczK@interia.pl>
2025-04-07 20:53:14 +0200

    changed structure of header-deleted tag <header> from nav and style navigation in css

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const mobileNav = document.querySelector('.nav__links--mobile')
 const openBtn = document.querySelector('.nav__btn--open')
 const closeBtn = document.querySelector('.nav__btn--close')
-const windowMedia = window.matchMedia('(min-width: 992px)')
+const windowMedia = window.matchMedia('(min-width: 768px)')
 const allMobileNavLinks = mobileNav.querySelectorAll('.nav__link')
 console.log(allMobileNavLinks)
 
@@ -22,7 +22,7 @@ const watchWindow = () => {
 	openBtn.classList.add('unactive')
 	closeBtn.classList.add('unactive')
 	mobileNav.classList.remove('nav__links--active')
-	if (window.innerWidth < 992) {
+	if (window.innerWidth < 768) {
 		openBtn.classList.remove('unactive')
 		closeBtn.classList.remove('unactive')
 		closeBtn.classList.remove('active')
